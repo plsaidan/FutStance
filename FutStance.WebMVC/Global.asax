6d5d9f174cc05587bd585bb1e1189d3556de2a38
Aidan Vandiver <skrtAidan@gmail.com>
2018-11-28 10:17:16 -0500

    Add project files.

---
+++ FutStance.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FutStance.WebMVC.MvcApplication" Language="C#" %>
---
+++ FutStance.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FutStance.WebMVC.Startup))]
+namespace FutStance.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
